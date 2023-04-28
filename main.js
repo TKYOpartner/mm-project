f7209cf89be40d7843d14de20b876cc654c41591
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-04-27 22:51:00 -0400

    Added function, proect complete

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const quotes = [
-    "If your dreams don’t scare you, they are too small.",
+    "If your dreams do not scare you, they are too small.",
     "Every accomplishment starts with the decision to try.",
     "Those who say it cannot be done should not interrupt those doing it.",
     "Never put off until tomorrow what you can do the day after tomorrow.",
@@ -7,9 +7,18 @@ const quotes = [
 ];
 
 const authors = [
-    "- Richard Branson",
-    "- John F Kennedy",
-    "- Chinese proverb",
-    "- Mark Twain",
-    "- Aristotle"
+    "Richard Branson",
+    "John F Kennedy",
+    "Chinese proverb",
+    "Mark Twain",
+    "Aristotle"
 ];
+
+function generateMessage() {
+    const quote = quotes[Math.floor(Math.random() * quotes.length)];
+    const author = authors[Math.floor(Math.random() * authors.length)];
+    return `"${quote}" - ${author}`;
+}
+
+
+console.log(generateMessage());
