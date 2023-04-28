34a0183c4315b2fe81134228efaff1e9d8d8236f
TKYOpartner <128873901+TKYOpartner@users.noreply.github.com>
2023-04-27 22:36:19 -0400

    Added quotes & authors

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 const quotes = [
-    "",
-    "",
-    "",
-    "",
-    ""
+    "If your dreams don’t scare you, they are too small.",
+    "Every accomplishment starts with the decision to try.",
+    "Those who say it cannot be done should not interrupt those doing it.",
+    "Never put off until tomorrow what you can do the day after tomorrow.",
+    "We are what we repeatedly do. Excellence then, is not an act, but a habit."
 ];
 
 const authors = [
-    "",
-    "",
-    "",
-    "",
-    ""
+    "- Richard Branson",
+    "- John F Kennedy",
+    "- Chinese proverb",
+    "- Mark Twain",
+    "- Aristotle"
 ];
